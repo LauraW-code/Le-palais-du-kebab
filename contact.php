5d963f28751643e766ed76d9f8ffb1c70e25170d
Laura WOLFF <laura.wolff@live.fr>
2023-10-05 09:58:49 +0200

    added all contact page files

---
+++ contact.php
@@ -0,0 +1,136 @@
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="contact2.css">
+    <link href="https://fonts.cdnfonts.com/css/montserrat" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/montez" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+    include 'data_contact.php';
+    ?>
+    <main>
+        <div class="dining-room">
+            <h1>Contactez-nous</h1>
+        </div>
+        <div class="contact-page">
+
+            <!-------------------------------------->
+            <!--/Partie gauche de la page contact/-->
+            <!-------------------------------------->
+            <div class="left-side">
+                <h2>Informations de contact</h2>
+                <p>Pour toutes questions ou demande, vous pouvez nous contacter directement par téléphone, par mail, ou
+                    via
+                    le
+                    formulaire de contact.</p>
+                <div class="contact-info">
+                    <div class="contact-left">
+                        <p>Téléphone</p>
+                        <p>Email</p>
+                        <p>Adresse</p>
+                    </div>
+                    <div class="contact-right">
+                        <p>03 54 44 94 32</p>
+                        <p>contact@palaiskebab.fr</p>
+                        <p>15 rue de Paris, 57160 Rozérieulles</p>
+                    </div>
+                </div>
+                <h3>Horaires d'ouverture:</h3>
+                <div class="contact-info">
+                    <div class="contact-left">
+                        <p>Dimanche au Vendredi</p>
+                        <p>Samedi</p>
+                    </div>
+                    <div class="contact-right">
+                        <p>11h30 - 23h</p>
+                        <p>11h30 - 1h</p>
+                    </div>
+                </div>
+
+            </div>
+            <!-------------------------------------->
+            <!--/Partie droite de la page contact/-->
+            <!-------------------------------------->
+            <form class="right-side" id="send-message" action="contact2.html" method="$_POST">
+                <h2>Formulaire de contact</h2>
+                <div class="contact-form">
+                    <input type="text" id="name" size="40" placeholder="Votre nom et prénom *" required>
+                    <input type="text" id="email" size="40" placeholder="Votre adresse email *" required>
+                    <select name="messageObject" id="subject" required>
+                        <option value="">Veuillez sélectionner...</option>
+                        <?php
+                        foreach ($messageObject as $index => $value){
+                            echo "<option value=" . $index . ">" . $value . "</option>";
+                        }?>
+                        <?php endforeach?>
+                    <textarea id="message" cols="40" rows="10" size="40" placeholder="Votre message *"
+                        required></textarea>
+                </div>
+                
+                <p>* Champs obligatoires</p>
+                <div class="check-checkbox">
+                    <input type="checkbox" id="conditions" required>
+                    <label for="conditions">En soumettant ce formulaire, j’accepte que les informations saisies soient
+                        utilisées
+                        pour permettre de me recontacter dans le cadre de la relation commerciale qui découle de cette
+                        demande
+                        d’information.</label>
+                </div>
+                <input type="submit" id="button-contact" value="ENVOYER">
+                <script>
+                    const message = "Votre message a bien été envoyé."
+
+                    document.getElementById("send-message").addEventListener("submit", function (event) {
+                        event.preventDefault();
+                        alert(message);
+                    });
+                </script>
+            </form>
+        </div>
+
+    </main>
+    <!--/Insertion googlemap/-->
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1552.9118022073992!2d6.090854781138993!3d49.11179656604232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4794db53139e85ab%3A0x68beb5ad5fe60462!2sLe%20Palais%20du%20Kebab!5e0!3m2!1sfr!2sfr!4v1695378466042!5m2!1sfr!2sfr"
+        class="map" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
---
+++ result_contact.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="contact2.css">
+        <link href="https://fonts.cdnfonts.com/css/montserrat" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/montez" rel="stylesheet">
+        <title>Récapitulatif</title>
+    </head>
+
+    <body class="result-contact-body">
+
+        <header class="result-contact-header">
+            <h1 class="return-title">Merci pour votre message<h1>
+        </header>
+        <?php
+
+        //var_dump($_POST);
+        $form=[$_POST];
+
+        ?>
+        <main class="result-contact-main">
+            <div class="result-contact-div">
+                
+                <?php 
+                foreach($form as $key=>$value):
+                ?>
+                <ul class="result-contact-ul">
+                    <li class="result-contact-li">Votre nom :  <?= $value['firstName']?></li>
+                    <li class="result-contact-li">Votre email :  <?= $value['email']?></li>
+                    <li class="result-contact-li">Objet de votre demande :  <?= $value['messageObject']?></li>
+                    <li class="result-contact-li">Votre message : <?= $value['message']?></li>     
+                </ul>
+                <?php endforeach?>
+
+                <button onclick="location.href='contact.php';" class="result-contact-button">Retour au site</button>
+            
+            </div>
+        </main>
+        
+    </body>
+</html>
