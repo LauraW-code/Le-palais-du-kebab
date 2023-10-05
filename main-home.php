fff5e1e1d4172d503db3678772a8f649a2b76eb5
antoninbrdl24 <antonin.brondel24@gmail.com>
2023-10-05 11:45:39 +0200

    "Nouvel ajout pour les fichiers de Pierre"

---
+++ main-home.php
@@ -0,0 +1,130 @@
+
+<body>
+    <div class="boite">
+        <main>
+            <div class="container-home">
+                <ul class="carousel-home">
+                    <li> <img src="pictures/carousel 4.jpg"> </li>
+                    <li> <img src="pictures/carousel 5.jpg"> </li>
+                    <li> <img src="pictures/carousel 6.jpg"> </li>
+                    <li> <img src="pictures/carousel 7.jpg"> </li>
+                </ul>
+            </div>
+            <script src="script-general2.js"> </script>
+            <br> <br>
+            <h2 class="titre2-home"> Bienvenu au Palais du Kebab </h2>
+            <br>
+            <br>
+            <p class="texte-home"> Votre repas 100% BIO vous attends avec impatience ! </p>
+            <br>
+            <br>
+
+            <!-- partial:index.partial.html -->
+
+
+
+            <section>
+                <p class="texte-home"> Créé à l'origine en 1990 par Yavuz, notre restaurant c'est immédiatement démarqué par
+                    la qualité et
+                    l'orgine bio de ses produits. </p>
+                <br>
+                <br>
+                <br>
+                <img
+                    src="pictures/2023-09-29 06_55_41-Premium Photo _ Photo a stunning shot of a business person with offical office a.png">
+                <br>
+            </section>
+            <section>
+                <br> <br>
+                <p class="texte-home"> Notre équipe est prête à vous accueillir dans le meilleur restaurant-kebab de
+                    ........................</p>
+                <br> <br>
+
+                <br>
+                <br>
+            </section>
+
+            </img>
+            <br>
+            <br>
+
+
+        </main>
+        <aside>
+
+            <a class="broche" href="resa.php">
+                <svg viewBox="0 0 51 302" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" width="60px">
+                    <!-- Generator: Sketch 3.7 (28169) - http://www.bohemiancoding.com/sketch -->
+                    <title>loading-kebab</title>
+                    <desc>Created with Sketch.</desc>
+                    <defs>
+                        <polygon id="path-1" points="9 0 11 4 7 4"></polygon>
+                        <mask id="mask-2" maskContentUnits="userSpaceOnUse" maskUnits="objectBoundingBox" x="-1" y="-1"
+                            width="6" height="6">
+                            <rect x="6" y="-1" width="6" height="6" fill="white"></rect>
+                            <use xlink:href="#path-1" fill="black"></use>
+                        </mask>
+                    </defs>
+                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g id="loading-kebab" transform="translate(0.000000, 1.000000)">
+                            <g id="skewer" transform="translate(17.000000, 0.000000)">
+                                <path d="M9,8 L9,179.855515" id="Line" stroke="#FFFFFF" stroke-width="6"
+                                    stroke-linecap="round"></path>
+                                <path d="M9,13 L9,7" id="Line" stroke="#FFFFFF" stroke-width="6"
+                                    stroke-linecap="square">
+                                </path>
+                                <g id="Triangle-1">
+                                    <use fill="#FFFFFF" fill-rule="evenodd" xlink:href="#path-1"></use>
+                                    <use stroke="#FFFFFF" mask="url(#mask-2)" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" xlink:href="#path-1"></use>
+                                </g>
+                                <path
+                                    d="M0,189 C0,193.970563 4.02943725,198 9,198 L9,198 C13.9705627,198 18,193.970563 18,189 C18,184.029437 13.9705627,180 9,180"
+                                    id="Oval-1" stroke="#FFFFFF" stroke-width="6" stroke-linecap="round"></path>
+                            </g>
+                            <g id="onion" class="food" transform="translate(5, 145)" stroke="#FFFFFF" stroke-width="4"
+                                fill="#3B3E40" stroke-linejoin="round">
+                                <path
+                                    d="M0,9.91380957 C0,9.91380957 12.2674491,14.6381008 18.5777778,15.5788436 C24.9341157,16.5264455 38,15.5788436 38,15.5788436 L32.9333333,2.12438777 C32.9333333,2.12438777 25.9304665,3.16065805 22.8,2.83251702 C19.174911,2.45252879 12.6666667,0 12.6666667,0 L0,9.91380957 Z"
+                                    id="Path-10"></path>
+                            </g>
+                            <g id="chicken-1" class="food" transform="translate(5.000000, 112.000000)" stroke-width="4"
+                                stroke="#FFFFFF" stroke-linejoin="round">
+                                <polygon id="Path-8" fill="#3B3E40"
+                                    points="1.82608696 0 0 18.72 21 26 42 21.84 40.173913 4.16"></polygon>
+                                <polyline id="Path-9"
+                                    points="0.913043478 17.3333333 28.3043478 13.8666667 41.0869565 20.8">
+                                </polyline>
+                            </g>
+                            <g id="tomato-1" class="food"
+                                transform="translate(25.719178, 91.698789) rotate(-6.000000) translate(-25.719178, -91.698789) translate(2.219178, 78.698789)"
+                                stroke="#FFFFFF" stroke-width="4" fill="#3B3E40">
+                                <path
+                                    d="M0.988209687,6.48774423 C0.988209687,17.2572969 11.2856604,25.9877442 23.9882097,25.9877442 L23.9882097,25.9877442 C36.6907589,25.9877442 46.9882097,17.2572969 46.9882097,6.48774423"
+                                    id="Oval-2"></path>
+                                <ellipse id="Oval-3" cx="24" cy="7" rx="23" ry="7"></ellipse>
+                            </g>
+                            <g id="chicken-2" class="food" transform="translate(8.000000, 47.000000)" stroke="#FFFFFF"
+                                stroke-width="4" stroke-linejoin="round">
+                                <polygon id="Path-5" fill="#3B3E40" points="0 9 2 22 28 24 37 2 12 0"></polygon>
+                                <polyline id="Path-6" stroke-linecap="round" points="0 9 12.6578947 12 37 2">
+                                </polyline>
+                            </g>
+                            <g id="tomato-2" class="food" transform="translate(14.000000, 17.000000)" stroke="#FFFFFF"
+                                stroke-width="4" fill="#3B3E40">
+                                <ellipse id="Oval-1" cx="12" cy="10.5" rx="12" ry="10.5"></ellipse>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+            </a>
+    </div>
+    <!-- partial -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js'></script>
+    <script src="./script-general.js"></script>
+    
+        <p class="texte-home"> Réservation </p>
+
+        </aside>
