9ea3de72fc396a2b08d8f28aedf1ea36e866b81e
Adrien Cremeaux <adrien.cremeaux@outlook.fr>
2023-10-05 12:12:03 +0200

    fichier adrien

---
+++ fournisseur.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fournisseurs</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montez&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Satisfy&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="smallscreen.css" media="screen and (max-width: 1000px)">
+    <link rel="stylesheet" href="mediumscreen.css" media="screen and (min-width: 768px) and (max-width: 2000px)">
+</head>
+
+<body>
+    <nav>
+
+        <h1>Le Palais du Kebab</h1>
+        <ul class="nav-menu">
+            <li><a class="nav-link" href="/menu.htlm">Menu </a></li>
+            <li><a class="nav-link" href="/Reservation.html">Réservation</a></li>
+            <li><a class="nav-link" href="/Engagements.html">Nos engagements</a></li>
+            <li><a class="nav-link" href="/team.html">La team</a></li>
+            <li><a class="nav-link" href="/contact.html">Contact</a></li>
+        </ul>
+
+    </nav>
+    <div class="top">
+        <h2> Nos fournisseurs ont du talent </h2>
+    </div>
+    <div class="tomate">
+        <div class="boxA">
+            <h3>Jeanne, productrice de tomates bio</h3>
+        </div>
+
+        <div class="boxB"><img class="image"
+                src="https://media.discordapp.net/attachments/1156580466166595644/1156580609704067132/swaglapine_photo_of_a_middle_aged_woman_picking_tomatoes_in_a_f_02025e2c-fb00-462d-8ff7-bcc56f9e8d0e.png?ex=651625e1&is=6514d461&hm=c13e72e88303670e0ea3cf87dccf7b3fa4221a3551048a02c5ff8556fb8a016c&=&width=676&height=676"
+                alt="Jeanne, agricultrice" /> </div>
+        <div class="boxC"><img class="image1"
+                src="https://media.discordapp.net/attachments/1156580466166595644/1156580670030762144/swaglapine_sun-kissed_basket_full_of_tomatoes_5901a362-e12d-4d4c-852f-6aec78f8db3f.png?ex=651625ef&is=6514d46f&hm=3e8a62ec4744e48ab20f684dddd12aa389cd794703223f59a36069d8a13d18a0&=&width=676&height=676"
+                alt="Champ de tomates" />
+        </div>
+        <div class="boxD">
+            <p class="bordertomate"> Savourez la fraîcheur des tomates San Marzano, parfaites pour chaque bouchée de nos
+                délicieux kebabs au Palais du Kebab. Leur saveur riche et leur texture juteuse rehaussent l'excellence
+                de nos plats.
+                <br>
+        </div>
+    </div>
+
+
+    <div class="salade">
+        <div class="boxE">
+            <h4>Serge, producteur de salades
+            </h4>
+        </div>
+        <div class="boxF"><img class="image"
+                src="https://media.discordapp.net/attachments/1156580466166595644/1156585665497153586/swaglapine_photograph_of_a_50_year_old_male_holding_a_lettuce_i_498fd539-f52a-4ea7-8e6e-c5bc4079d610.png?ex=65162a96&is=6514d916&hm=41fdc0acb18714747cc9734721786c3fa6a07f92af2170386d015c02c9846ba8&=&width=676&height=676"
+                alt="champ de salade" /></div>
+        <div class="boxG"><img class="image1"
+                src="https://media.discordapp.net/attachments/1156580466166595644/1156586208143614043/swaglapine_fresh_lettuce_in_a_restaurant_kitchen_72053ae1-9323-4d1a-9014-caa15162180b.png?ex=65162b18&is=6514d998&hm=1ad98f19ad67644f8ada3c72043b306b2a826eda71660c106c30455518567f89&=&width=676&height=676"
+                alt="champ de salade" /></div>
+        <div class="boxH">
+            <p class="bordersalade"> Découvrez la perfection du kebab chez "Le Palais du Kebab" avec nos salades
+                fraîches de chez Serge. Des ingrédients locaux de qualité pour une expérience
+                délicieuse. Bon appétit ! </p>
+        </div>
+    </div>
+
+
+    <div class="pain">
+        <div class="boxI">
+            <h5> Roberto, boulanger traditionnel</h5>
+        </div>
+        <div class="boxJ"><img class="image"
+                src="https://media.discordapp.net/attachments/1156580466166595644/1156596101474951178/swaglapine_baker_preparing_raw_kabob_dread_dough_7e084797-c4fa-4824-8f49-a4729b5a3147.png?ex=6516344f&is=6514e2cf&hm=9d4ad34f1f3d846ee6725317f1b901d4c1067b2b0ed3408e3f250d8d0eb583df&=&width=676&height=676"
+                alt="pain du kebab" /></div>
+        <div class="boxK"><img class="image1"
+                src="https://media.discordapp.net/attachments/1156580466166595644/1156605791491604480/swaglapine_None_4bebd864-00ff-4514-a19d-5217f52f4f48.png?ex=65163d55&is=6514ebd5&hm=84415a150927b429fca45dffbd593ba6fba53db5f2252b8a01e2b315ba9ea3f2&=&width=676&height=676"
+                alt="champ de salade" /></div>
+        <div class="boxL">
+            <p class="borderpain"> Notre boulanger traditionnel au Palais du Kebab façonne avec passion des pains
+                moelleux, ajoutant une touche artisanale irrésistible à chaque kebab que nous proposons. Découvrez la
+                fusion parfaite entre tradition et délicieux kebabs.
+            </p>
+        </div>
+    </div>
+
+
+    <div class="viande">
+        <div class="boxM">
+            <h6>Yavuz, notre petit père du kebab
+            </h6>
+        </div>
+        <div class="boxO"><img class="image"
+                src="https://media.discordapp.net/attachments/1156580466166595644/1159428103978893442/mash_up_yavuz.png?ex=6530fc91&is=651e8791&hm=5906a6601b441f8c1243293d0563ce4f51c273b7a2625abf585b4fc99fa32a9c&=&width=676&height=676www" />
+        </div>
+        <div class="boxN"><img class="image1"
+                src="https://media.discordapp.net/attachments/1156580466166595644/1157304076221022228/swaglapine_None_2f9b986e-2f40-4e0e-a6bc-e4a00370fce0.png?ex=651e0da9&is=651cbc29&hm=60dd335a42c5c82930ad8bb4232d0485096418567308404d6ee79908d5fa48d5&=&width=676&height=676"
+                alt="pain du kebab" />
+        </div>
+        <div class="boxP">
+            <p class="borderviande">Yavuz, notre talentueux expert en viande marinée au Palais du Kebab, insuffle à
+                chaque bouchée de nos délicieux kebabs une passion authentique. Chaque morceau tournant sur la broche
+                est une expérience culinaire unique.</p>
+        </div>
+    </div>
+
+
+    <div class="frite">
+        <div class="boxQ">
+            <h6>Kevin, notre artiste de la frite</h6>
+        </div>
+        <div class="boxR"><img class="image"
+                src="https://media.discordapp.net/attachments/1156580466166595644/1157260386110816306/swaglapine_photograph_of_a_30_year_old_man_alone_cutting_french_0e904a70-6e5d-4292-a296-c28d20aea8da.png?ex=651de4f8&is=651c9378&hm=96c33f5497ffaa7c54d5eec600455d9663b96954cf5a85600a24ffbc61e0ae00&=&width=676&height=676"
+                alt="pain du kebab" /></div>
+        <div class="boxS"><img class="image1"
+                src="https://media.discordapp.net/attachments/1156580466166595644/1157258468676337664/swaglapine_photograph_of_a_plate_full_of_golden_french_fries_495ee0f3-aa47-47c2-9d1c-65b9c4c88015.png?ex=651de32f&is=651c91af&hm=535743117e57996866ca09ac2c8adaf69b888342a14beea77720a30d83ec6e7f&=&width=676&height=676"
+                alt="pain du kebab" /></div>
+        <div class="boxT">
+            <p class="borderfrite">
+                Savourer nos frites maison au Palais du Kebab, c'est déguster l'expertise de Kevin. Croustillantes à
+                l'extérieur, moelleuses à l'intérieur, elles sont préparées avec soin dans du blanc de bœuf, ajoutant
+                une touche savoureuse à chaque bouchée.
+            </p>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
